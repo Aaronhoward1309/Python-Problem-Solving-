afa0841d2f5e148d858210c84b9c2b2e8957ce19
Aaron Howard <aaronhoward5139@gmail.com>
2026-05-06 21:35:46 +0100

    Add files via upload

---
+++ ajh1.py
@@ -0,0 +1,17 @@
+
+for i in range(1,5,):
+    print(i)
+    
+
+def get_half(num):
+    num=num/2
+    return num 
+print(get_half(50))
+
+fruits= {
+    "apple":1,
+    "banna":1,
+    "cherry":1
+    }
+print(fruits)
+
